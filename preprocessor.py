f0d5250344b06d1280f03d4cfacc4555db47938a
Adit Sinsinwal <asinsinw@uwaterloo.ca>
2025-08-04 22:46:59 -0400

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -1,7 +1,7 @@
 import re
 
 def clean_ocr_text(text):
-    # Remove long blank lines or repetitive headers/footers
+    # to remove long blank lines or repetitive headers and footers
     text = re.sub(r'\n{2,}', '\n', text)
     text = re.sub(r'Page \\d+ of \\d+', '', text)
     return text.strip()
